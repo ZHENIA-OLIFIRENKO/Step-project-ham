be8470b1a9bf60b652a26f5c4eb6ee11d6453042
Yevheniy Olifirenko <8olifirenko@gmail.com>
2023-10-07 18:46:38 +0300

    development of the "Our Amazing Work" block 1/2

--- script/script.js
+++ script/script.js
@@ -13,3 +13,29 @@ let tabBlock = function () {
     }
 };
 tabBlock();
+
+// ========================================
+
+document.addEventListener("DOMContentLoaded", function () {
+    function filterImages(category) {
+        const imgItems = document.querySelectorAll('.img-item');
+        imgItems.forEach(item => {
+            const dataBlockImg = item.getAttribute('data-block-img');
+                if (dataBlockImg === category || category === 'all') {
+                    item.style.display = 'block';
+                } else {
+                    item.style.display = 'none';
+                }
+        });
+    }
+
+    const tabs = document.querySelectorAll('.tabs-img');
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function () {
+            const category = this.getAttribute('data-block-img');
+            tabs.forEach(t => t.classList.remove('active'));
+            this.classList.add('active');
+            filterImages(category);
+        });
+    });
+});
